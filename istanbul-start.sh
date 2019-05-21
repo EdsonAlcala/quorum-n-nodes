29b3cc60780e9f5bf780cd0324cc1f62d362d0c8
Hugh Livingstone <hugh.livingstone@consensys.net>
2019-05-21 20:10:28 +0100

    Added rpcvhosts to allow connection with any hostname

--- istanbul-start.sh
+++ istanbul-start.sh
@@ -4,7 +4,7 @@
 
 ARGS_NOMINER="--nodiscover --istanbul.blockperiod 1 --syncmode full --verbosity 5 --debug --metrics --gasprice 0 --nat none"
 ARGS="--nodiscover --istanbul.blockperiod 1 --syncmode full --mine --minerthreads 1 --verbosity 5 --debug --metrics --gasprice 0 --targetgaslimit 804247552 --nat none"
-RPC_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul"
+RPC_ARGS='--rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul --rpcvhosts "*"'
 
 #### Start node 1 #######################
 geth --datadir qdata_1/dd init genesis.json
